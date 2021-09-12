c551dd53ce84f832c0d4aac678d8a4588e7a4d1d
baran <dasdemirbaran@gmail.com>
2021-09-12 12:14:30 +0300

    API: prepare app to serve built lib aswell

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
@@ -11,6 +13,7 @@ app.use(morgan('dev', { skip: () => process.env.NODE_ENV === 'test' }));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
